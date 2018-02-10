b8b0d2cf4d9101810cf1578e487810e620e00778
Michal Augustyn <augustyn.michal@gmail.com>
2018-02-10 17:28:08 +0100

    implement the forwarding application in pure Java

--- src/main/scala/protocbridge/frontend/WindowsPluginFrontend.scala
+++ src/main/scala/protocbridge/frontend/WindowsPluginFrontend.scala
@@ -1,15 +1,15 @@
 package protocbridge.frontend
 
+import java.net.ServerSocket
 import java.nio.file.{Files, Path}
 
 import protocbridge.ProtocCodeGenerator
 
-import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
-import java.net.{ServerSocket, Socket}
+import scala.concurrent.Future
 
 /** A PluginFrontend that binds a server socket to a local interface. The plugin
-  * is a batch script that invokes WindowsPluginFrontend.main method, in a new JVM with the same parameters
+  * is a batch script that invokes BridgeApp.main() method, in a new JVM with the same parameters
   * as the currently running JVM. The plugin will communicate its stdin and stdout to this socket.
   *
   * @param deprecatedPythonExecutable Not used now, it's here to keep binary compatibility.
@@ -40,23 +40,8 @@ class WindowsPluginFrontend(deprecatedPythonExecutable: String = "") extends Plu
   private def createWindowsScript(port: Int): InternalState = {
     val batchFile = PluginFrontend.createTempFile(".bat",
       s"""@echo off
-          |"${sys.props("java.home")}\\bin\\java.exe" -cp "${sys.props("java.class.path")}" ${WindowsPluginFrontend.getClass.getName.stripSuffix("$")} $port
+          |"${sys.props("java.home")}\\bin\\java.exe" -cp "${getClass.getProtectionDomain.getCodeSource.getLocation.getPath}" ${classOf[BridgeApp].getName} $port
         """.stripMargin)
     InternalState(batchFile)
-  }
-}
-
-object WindowsPluginFrontend {
-  def main(args: Array[String]): Unit = {
-    val port = args.head.toInt
-    val socket = new Socket("127.0.0.1", port)
-    try {
-      // read stdin and write it to the socket
-      val input = PluginFrontend.readInputStreamToByteArray(System.in)
-      socket.getOutputStream.write(input)
-      socket.shutdownOutput()
-      // read the socket and write bytes to stdout
-      System.out.write(PluginFrontend.readInputStreamToByteArray(socket.getInputStream))
-    } finally socket.close()
   }
 }

84d64f9294c7a3fcd09b40fd58af6f93218bdf68
Bell Le <bell.le@databricks.com>
2024-09-13 10:09:34 -0700

    Switch to MacPluginFrontend in tests

--- bridge/src/test/scala/protocbridge/frontend/PosixPluginFrontendSpec.scala
+++ bridge/src/test/scala/protocbridge/frontend/PosixPluginFrontendSpec.scala
@@ -3,11 +3,11 @@ package protocbridge.frontend
 class PosixPluginFrontendSpec extends OsSpecificFrontendSpec {
   if (!PluginFrontend.isWindows && !PluginFrontend.isMac) {
     it must "execute a program that forwards input and output to given stream" in {
-      testSuccess(PosixPluginFrontend)
+      testSuccess(MacPluginFrontend)
     }
 
     it must "not hang if there is an OOM in generator" in {
-      testFailure(PosixPluginFrontend)
+      testFailure(MacPluginFrontend)
     }
   }
 }

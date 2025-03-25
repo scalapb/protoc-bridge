ae484c85e2ee2a8abc68053aa1f21d2e4390da8f
reimai <naugadina@gmail.com>
2025-03-25 16:40:21 +0300

    fix getting a free socket on macos

--- bridge/src/main/scala/protocbridge/frontend/SocketBasedPluginFrontend.scala
+++ bridge/src/main/scala/protocbridge/frontend/SocketBasedPluginFrontend.scala
@@ -1,22 +1,25 @@
 package protocbridge.frontend
 
+import protocbridge.frontend.SocketBasedPluginFrontend.getFreeSocket
 import protocbridge.{ExtraEnv, ProtocCodeGenerator}
 
-import java.net.ServerSocket
+import java.net.{InetAddress, ServerSocket}
 import java.nio.file.{Files, Path}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.{Future, blocking}
 
+
 /** PluginFrontend for Windows and macOS where a server socket is used.
-  */
+ */
 abstract class SocketBasedPluginFrontend extends PluginFrontend {
+
   case class InternalState(serverSocket: ServerSocket, shellScript: Path)
 
   override def prepare(
-      plugin: ProtocCodeGenerator,
-      env: ExtraEnv
-  ): (Path, InternalState) = {
-    val ss = new ServerSocket(0) // Bind to any available port.
+                        plugin: ProtocCodeGenerator,
+                        env: ExtraEnv
+                      ): (Path, InternalState) = {
+    val ss = getFreeSocket()
     val sh = createShellScript(ss.getLocalPort)
 
     Future {
@@ -48,4 +51,22 @@ abstract class SocketBasedPluginFrontend extends PluginFrontend {
   }
 
   protected def createShellScript(port: Int): Path
+}
+
+
+object SocketBasedPluginFrontend {
+
+  /**
+   * for mac we need to specify an address, otherwise it uses ANY (*.* in netstat)
+   * which does not conflict with existing 'localhost' sockets,
+   * resulting in a conflict later on in MacPluginFrontend
+   */
+  def getFreeSocket(): ServerSocket = {
+    if (!PluginFrontend.isMac) {
+      // Bind to any available port.
+      new ServerSocket(0)
+    } else {
+      new ServerSocket(0, 50, InetAddress.getByName("127.0.0.1"))
+    }
+  }
 }

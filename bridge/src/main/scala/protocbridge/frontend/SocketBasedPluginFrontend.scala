80be6541bae4338da775990f8b4c3e21a3e5caad
reimai <naugadina@gmail.com>
2025-04-10 12:24:07 +0300

    free socket for windows; fmt

--- bridge/src/main/scala/protocbridge/frontend/SocketBasedPluginFrontend.scala
+++ bridge/src/main/scala/protocbridge/frontend/SocketBasedPluginFrontend.scala
@@ -1,6 +1,5 @@
 package protocbridge.frontend
 
-import protocbridge.frontend.SocketBasedPluginFrontend.getFreeSocket
 import protocbridge.{ExtraEnv, ProtocCodeGenerator}
 
 import java.net.{InetAddress, ServerSocket}
@@ -8,18 +7,22 @@ import java.nio.file.{Files, Path}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.{Future, blocking}
 
-
 /** PluginFrontend for Windows and macOS where a server socket is used.
- */
+  */
 abstract class SocketBasedPluginFrontend extends PluginFrontend {
 
   case class InternalState(serverSocket: ServerSocket, shellScript: Path)
 
   override def prepare(
-                        plugin: ProtocCodeGenerator,
-                        env: ExtraEnv
-                      ): (Path, InternalState) = {
-    val ss = getFreeSocket()
+      plugin: ProtocCodeGenerator,
+      env: ExtraEnv
+  ): (Path, InternalState) = {
+
+    /** we need to specify an address, otherwise it uses ANY (*.* in netstat)
+      * which does not conflict with existing 'localhost' sockets on macos,
+      * resulting in a conflict later on in MacPluginFrontend
+      */
+    val ss = new ServerSocket(0, 50, InetAddress.getByName("127.0.0.1"))
     val sh = createShellScript(ss.getLocalPort)
 
     Future {
@@ -51,22 +54,4 @@ abstract class SocketBasedPluginFrontend extends PluginFrontend {
   }
 
   protected def createShellScript(port: Int): Path
-}
-
-
-object SocketBasedPluginFrontend {
-
-  /**
-   * for mac we need to specify an address, otherwise it uses ANY (*.* in netstat)
-   * which does not conflict with existing 'localhost' sockets,
-   * resulting in a conflict later on in MacPluginFrontend
-   */
-  def getFreeSocket(): ServerSocket = {
-    if (!PluginFrontend.isMac) {
-      // Bind to any available port.
-      new ServerSocket(0)
-    } else {
-      new ServerSocket(0, 50, InetAddress.getByName("127.0.0.1"))
-    }
-  }
 }
